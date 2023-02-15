b086c7dd7cc0063a6b5534982726a0d97c0f3436
shengjiangfeng <max.sheng@splashtop.com>
2023-02-15 23:39:47 +0800

    chore: add reconcile for application-operator

--- api/v1/groupversion_info.go
+++ api/v1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1 contains API Schema definitions for the apps v1 API group
-//+kubebuilder:object:generate=true
-//+groupName=apps.danielhu.cn
+// +kubebuilder:object:generate=true
+// +groupName=apps.danielhu.cn
 package v1
 
 import (
--- controllers/application_controller.go
+++ controllers/application_controller.go
@@ -18,13 +18,18 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"time"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
-	appsv1 "github.com/shengjiangfeng/application-operator/api/v1"
+	dappsv1 "github.com/shengjiangfeng/application-operator/api/v1"
 )
 
 // ApplicationReconciler reconciles a Application object
@@ -47,16 +52,41 @@ type ApplicationReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.12.2/pkg/reconcile
 func (r *ApplicationReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
-
-	// TODO(user): your logic here
-
+	l := log.FromContext(ctx)
+	// get the application
+	app := &dappsv1.Application{}
+	if err := r.Get(ctx, req.NamespacedName, app); err != nil {
+		if errors.IsNotFound(err) {
+			l.Info("the Application not found")
+			return ctrl.Result{}, err
+		}
+		l.Error(err, "failed to get the Application")
+		return ctrl.Result{}, err
+	}
+	// create pods
+	for i := 0; i < int(app.Spec.Replicas); i++ {
+		pod := &corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      fmt.Sprintf("%s-%d", app.Name, i),
+				Namespace: app.Namespace,
+				Labels:    app.Labels,
+			},
+			Spec: app.Spec.Template.Spec,
+		}
+		if err := r.Create(ctx, pod); err != nil {
+			l.Error(err, "failed to create pod for application")
+			return ctrl.Result{RequeueAfter: 1 * time.Minute}, err
+		}
+		l.Info(fmt.Sprintf("the pod %s has been created", pod.Name))
+	}
+	l.Info("all pods has created")
 	return ctrl.Result{}, nil
+
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *ApplicationReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&appsv1.Application{}).
+		For(&dappsv1.Application{}).
 		Complete(r)
 }
